ba582abd4ff8accc5424f1a0989af2c3e2669a1f
null-routed <55241343+null-routed@users.noreply.github.com>
2023-05-13 19:06:56 +0200

    Fixed formatting

--- include/SecureBankApplication/Constants.h
+++ include/SecureBankApplication/Constants.h
@@ -1,12 +1,13 @@
 #pragma once
+#include <openssl/evp.h>
 
 namespace Constants {
-    const int MAX_BUFFER_SIZE = 1024; // Maximum buffer size for recv
-    const int USER_ID_LENGTH = 16; // User ID length, not sure if necessary
-    const int SERVER_PORT = 8080; // The port the server will use to handle incoming connections
-    const int INIT_SESSION = 0; // Commands
-    const int LOGIN = 1; 
-    const int TRANSFER = 2; 
-    const int GET_BALANCE = 3;
-    const int GET_TRANSFER_HISTORY = 4; 
+    constexpr int MAX_BUFFER_SIZE = 1024; // Maximum buffer size for recv
+    constexpr int USER_ID_LENGTH = 16; // User ID length, not sure if necessary
+    constexpr int SERVER_PORT = 8080; // The port the server will use to handle incoming connections
+    constexpr int INIT_SESSION = 0; // Commands
+    constexpr int LOGIN = 1; 
+    constexpr int TRANSFER = 2; 
+    constexpr int GET_BALANCE = 3;
+    constexpr int GET_TRANSFER_HISTORY = 4; 
 }
