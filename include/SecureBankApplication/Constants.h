0b7fe9ef0ce9e558f0635dccce7862595c5e4380
null-routed <55241343+null-routed@users.noreply.github.com>
2023-05-17 10:13:53 +0200

    Added more constants (TIMEOUT), removed unused imports

--- include/SecureBankApplication/Constants.h
+++ include/SecureBankApplication/Constants.h
@@ -2,8 +2,9 @@
 #include <openssl/evp.h>
 
 namespace Constants {
+    constexpr int TIMEOUT_TIME = 5 * 60;
     constexpr int SALT_SIZE = 16;
-    constexpr int MAX_BUFFER_SIZE = 1024; // Maximum buffer size for recv
+    constexpr int MAX_BUFFER_SIZE = 4096; // Maximum buffer size for recv
     constexpr int USER_ID_LENGTH = 16; // User ID length, not sure if necessary
     constexpr int SERVER_PORT = 8080; // The port the server will use to handle incoming connections
     constexpr int INIT_SESSION = 0; // Commands
