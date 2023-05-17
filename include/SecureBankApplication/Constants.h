768814d53cf2c14fee324823fa8c602c90110bef
null-routed <55241343+null-routed@users.noreply.github.com>
2023-05-17 15:41:20 +0200

    Added header size

--- include/SecureBankApplication/Constants.h
+++ include/SecureBankApplication/Constants.h
@@ -1,12 +1,15 @@
 #pragma once
-#include <openssl/evp.h>
 
 namespace Constants {
     constexpr int TIMEOUT_TIME = 5 * 60;
+
+    constexpr int HEADER_SIZE = 64;
     constexpr int SALT_SIZE = 16;
     constexpr int MAX_BUFFER_SIZE = 4096; // Maximum buffer size for recv
+
     constexpr int USER_ID_LENGTH = 16; // User ID length, not sure if necessary
     constexpr int SERVER_PORT = 8080; // The port the server will use to handle incoming connections
+
     constexpr int INIT_SESSION = 0; // Commands
     constexpr int LOGIN = 1; 
     constexpr int TRANSFER = 2; 
