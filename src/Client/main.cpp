e0a34d1a0d8ce0f56d9e6594eef047850b006cef
Maksymilian <makaleder@gmail.com>
2023-06-01 15:54:51 +0200

    Added handle conection to main

--- src/Client/main.cpp
+++ src/Client/main.cpp
@@ -8,6 +8,7 @@ int main()
     cout << "Welcome to NM Bank!" << endl;
     Client client = Client();
     client.connect_with_server();
+    client.handle_server_connection();
     client.destroy_session_keys();
     return 0;
 }
