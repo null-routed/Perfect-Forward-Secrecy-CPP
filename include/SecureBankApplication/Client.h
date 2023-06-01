81501069727aefaef7c3748969c7897832e74073
null-routed <55241343+null-routed@users.noreply.github.com>
2023-06-01 14:57:17 +0200

    Fixed minor errors in main.cpp for client

--- include/SecureBankApplication/Client.h
+++ include/SecureBankApplication/Client.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include "Transaction.h"
 #include <openssl/x509.h>
+// #include "Constants.h"
 
 struct Session
 {
--- src/Client/main.cpp
+++ src/Client/main.cpp
@@ -1,14 +1,5 @@
-#include "../../include/SecureBankApplication/Client.h"
-#include "../../include/SecureBankApplication/Transaction.h"
-#include "../../include/SecureBankApplication/Constants.h"
-#include "../Utils.cpp"
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
-#include <map>
-#include <iostream> 
-#include <vector>
-#include <string>
+#include "Client.h"
+#include <iostream>
 
 using namespace std;
 
